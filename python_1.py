a77b989dfde1d12c6f9fd4533243e31edb7a5368
priscilla nzula <danielpriscilla61@gmail.com>
2025-03-16 01:46:59 +0300

    Delete python_1.py

--- python_1.py
+++
@@ -1,8 +0,0 @@
-def python_program():
-    num1=int(input("Enter a number: "))
-    num2= int(input("Enter another number: "))
-    operation = num1+num2
-    print(f"{num1} + {num2} = {operation}")
-
-
-python_program()
